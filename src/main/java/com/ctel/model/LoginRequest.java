9ff109926d453bb23c76c4ce010950fb22a66a2c
devakumar7 <112143989+devakumar7@users.noreply.github.com>
2023-11-16 15:51:00 +0530

    Add files via upload

---
+++ src/main/java/com/ctel/model/LoginRequest.java
@@ -0,0 +1,30 @@
+package com.ctel.model;
+
+public class LoginRequest {
+
+	private String emailId;
+
+	private String password;
+
+	public LoginRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
